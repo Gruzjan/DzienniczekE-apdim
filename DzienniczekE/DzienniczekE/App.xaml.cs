6cfac171badb1cc0977a44ec893b1b7f27f1048d
Gruzjan <gracjan122@protonmail.com>
2022-12-06 21:42:34 +0100

    some shit idk anymore

--- DzienniczekE/DzienniczekE/App.xaml.cs
+++ DzienniczekE/DzienniczekE/App.xaml.cs
@@ -10,7 +10,7 @@ namespace DzienniczekE
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
